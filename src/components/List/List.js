2b3a17fa76b8d3e9569eebe6a6038154d3c14ed3
Bartek K <kontener00@gmail.com>
2020-09-12 07:20:49 +0200

    Adding new columns - props usage section

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js'
+import PropTypes from 'prop-types';
+import {settings} from '../../data/dataStore';
+
 
 class List extends React.Component {
+    static propTypes = {
+        title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
+    }
+
+    static defaultProps = {
+        children: <p>I can do all the things!!!</p>,
+    }
+
     render() {
         return (
             <section className={styles.component}>
                 {/* <h2 className={styles.subtitle}>Organize your world</h2> */}
-                <Hero />
+                <Hero titleText={this.props.title} />
             </section>
+
+            <div className={styles.columns}>
+
+            </div>
         )
     }
 }
