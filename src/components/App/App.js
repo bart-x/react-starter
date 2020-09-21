ed6632ddacf67fe8ae477e55a0141d92de0c98b9
Bartek K <kontener00@gmail.com>
2020-09-21 20:36:24 +0200

    Add switching between page view (17.3)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './App.scss';
 import Home from '../Home/HomeContainer';
 import Info from '../Info/Info';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
 import FAQ from '../FAQ/FAQ';
+import { AnimatedSwitch } from 'react-router-transition';
 
 const App = () => (
 
@@ -20,9 +22,16 @@ const App = () => (
   <div>
     <BrowserRouter>
       <MainLayout>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/info' component={Info} />
-        <Route exact path='/faq' component={FAQ} />
+        <AnimatedSwitch
+          atEnter={{ opacity: 0 }}
+          atLeave={{ opacity: 0 }}
+          atActive={{ opacity: 1 }}
+          className={styles.switchWrapper}
+        >
+          <Route exact path='/' component={Home} />
+          <Route exact path='/info' component={Info} />
+          <Route exact path='/faq' component={FAQ} />
+        </AnimatedSwitch>
       </MainLayout>
     </BrowserRouter>
   </div>
